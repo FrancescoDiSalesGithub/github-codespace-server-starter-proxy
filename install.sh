095e09712bd2d9fe6dae0a567925240ce23209a9
Francesco Di Sales <f.disales@hotmail.com>
2023-10-09 21:04:37 +0000

    adding ngrok

--- install.sh
+++ install.sh
@@ -16,4 +16,23 @@ sudo cp squid.conf /etc/squid/
 echo "starting squid proxy"
 sudo service squid start
 
+echo "installing ngrok"
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+
+echo "extracting ngrok"
+tar -xzvf ngrok-v3-stable-linux-amd64.tgz
+
+echo "adding auth token"
+
+if [ $1 == "" ]
+then
+    echo "please pass as argument your ngrok auth token"
+else
+    ./ngrok config add-authtoken 2NFDA7FBMluRcJwxFwnaOnq9SPX_5ymqWhtnu1sAoEn4Y9sP
+    echo "auth token saved"
+fi
+
+echo "starting ngrok"
+./ngrok tcp 3128
+
 
