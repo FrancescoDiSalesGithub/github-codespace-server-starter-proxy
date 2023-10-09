551ad3cae3fd3e787b9297900e7cac2df454787c
Francesco Di Sales <f.disales@hotmail.com>
2023-10-09 20:58:28 +0000

    updating install.sh

--- install.sh
+++ install.sh
@@ -4,26 +4,16 @@ echo "installing squid"
 
 sudo apt install -y squid
 
-echo "add the following settings to your squid proxy configuration file"
-echo "http_port 3128"
-echo "cache_dir /var/cache/squid 100 16 256"
-echo "acl all src 0.0.0.0/0"
-echo "http_access allow all"
-
-echo "squid file config created, please put the informations above"
-touch squid.conf
+echo "add the following settings to your squid proxy configuration file " 
+echo "http_port 3128 " > squid.conf
+echo "cache_dir /var/cache/squid 100 16 256 " >> squid.conf
+echo "acl all src 0.0.0.0/0 " >> squid.conf
+echo "http_access allow all " >> squid.conf
 
 echo "after putting config in the squid.conf file, plase put the file in /etc/squid"
+sudo cp squid.conf /etc/squid/
 
-echo "installing ngrok"
-wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-tar -xzvf https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-
-echo "put your access token for ngrok, you can find it as soon you log on https://dashboard.ngrok.com/get-started/setup"
-echo "./ngrok config add-authtoken YOUR_AUTH_TOKEN"
-
-echo "sudo service squid start"
-echo "./ngrok tcp 3128"
-
+echo "starting squid proxy"
+sudo service squid start
 
 
